a764d6fe12e64e7aba2f37e8dc596abe3f034c1f
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-07-13 23:57:39 +0530

    work manager foreground

--- app/src/main/java/com/example/workmanagerdemo/CustomWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo/CustomWorker.kt
@@ -1,14 +1,22 @@
 package com.example.workmanagerdemo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.ServiceInfo
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.Data
+import androidx.work.ForegroundInfo
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import okhttp3.internal.notify
 import javax.inject.Inject
 
 @HiltWorker
@@ -19,6 +27,7 @@ class CustomWorker @AssistedInject constructor(
 ): CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result {
         return try {
+            setForeground(getForegroundInfo(applicationContext))
             val response = api.getPost()
             if (response.isSuccessful) {
                 Log.d("CustomWorker","Success ${response.body()?.id} ${response.body()?.title}")
@@ -33,4 +42,42 @@ class CustomWorker @AssistedInject constructor(
         }
         return Result.success()
     }
+
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        return getForegroundInfo(applicationContext)
+    }
+}
+
+private fun getForegroundInfo(context: Context): ForegroundInfo{
+    return ForegroundInfo(
+        1,
+        createNotification(context),
+        ServiceInfo.FOREGROUND_SERVICE_TYPE_NONE
+    )
+}
+
+private fun createNotification(context: Context): Notification {
+
+    val channelId = "main_channel_id"
+    val channelName = "main channel"
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentTitle("Notification title")
+        .setContentText("This is my first text")
+        .setOngoing(true)
+//        .setAutoCancel(true)
+
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+         val channel =NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+         notificationManager.createNotificationChannel(channel)
+    }
+
+    return  builder.build()
+
 }
--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -10,33 +10,50 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.work.BackoffPolicy
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import com.example.workmanagerdemo.ui.theme.WorkManagerDemoTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.Duration
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val workRequest = OneTimeWorkRequestBuilder<CustomWorker>()
-            .setInitialDelay(Duration.ofSeconds(10))
-            .setBackoffCriteria(
-                backoffPolicy = BackoffPolicy.LINEAR,
-                duration = Duration.ofSeconds(15)
-            )
-            .build()
-
-        WorkManager.getInstance(applicationContext).enqueue(workRequest)
-
         setContent {
+
             WorkManagerDemoTheme {
+
+                val permission = rememberPermissionState(permission = android.Manifest.permission.POST_NOTIFICATIONS)
+
+                LaunchedEffect(key1 = Unit) {
+                    if (permission.status.isGranted) {
+                        val workRequest = OneTimeWorkRequestBuilder<CustomWorker>()
+                            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+//                          .setInitialDelay(Duration.ofSeconds(10))
+                            .setBackoffCriteria(
+                                backoffPolicy = BackoffPolicy.LINEAR,
+                                duration = Duration.ofSeconds(15)
+                            )
+                            .build()
+
+                        WorkManager.getInstance(applicationContext).enqueue(workRequest)
+                    } else {
+                        permission.launchPermissionRequest()
+                    }
+                }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
